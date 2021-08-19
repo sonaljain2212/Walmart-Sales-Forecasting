be93fc8db15d5620816da26233ba54c451f05f23
sonaljain2212 <51110015+sonaljain2212@users.noreply.github.com>
2021-08-19 13:50:50 -0400

    Update WSF_streamlit.py

--- WSF_streamlit.py
+++ WSF_streamlit.py
@@ -1,10 +1,11 @@
 #Imports
+import matplotlib
+import matplotlib.pyplot as plt
 import streamlit as st
 import pandas as pd
 import time
 import pandas as pd
-import matplotlib
-import matplotlib.pyplot as plt
+
 import numpy as np
 from itertools import cycle, islice
 import statsmodels.api as sm
